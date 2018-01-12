967efb66d7e6eaba828ebab69ac20615537311b0
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-12 13:20:47 +0100

    trying to remove add event listener

--- ejercicios/EjercicioJS/ejercio17arkaitz.js
+++ ejercicios/EjercicioJS/ejercio17arkaitz.js
@@ -9,3 +9,15 @@ document.getElementById("segundo").addEventListener("click", function(borrar){
 document.getElementById("tercero").addEventListener("click", function(borrar){
     document.getElementById("parafo3").style.display = "none"
 });
+
+document.getElementById("primero").removeEventListener("click", function(borrar){
+    document.getElementById("parafo1").style.display = "none"
+});
+
+document.getElementById("segundo").removeEventListener("click", function(borrar){
+    document.getElementById("parafo2").style.display = "none"
+});
+
+document.getElementById("tercero").removeEventListener("click", function(borrar){
+    document.getElementById("parafo3").style.display = "none"
+});
