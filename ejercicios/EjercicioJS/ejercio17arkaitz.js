5fc9bac1eb90bf0a79b413665a43a8a3476ca134
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-12 13:18:08 +0100

    Adding same functions for other paragraphs

---
+++ ejercicios/EjercicioJS/ejercio17arkaitz.js
@@ -0,0 +1,11 @@
+document.getElementById("primero").addEventListener("click", function(borrar){
+    document.getElementById("parafo1").style.display = "none"
+});
+
+document.getElementById("segundo").addEventListener("click", function(borrar){
+    document.getElementById("parafo2").style.display = "none"
+});
+
+document.getElementById("tercero").addEventListener("click", function(borrar){
+    document.getElementById("parafo3").style.display = "none"
+});
