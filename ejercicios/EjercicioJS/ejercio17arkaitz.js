ac43a6081d1e988eae214616c6acb14901aa7a50
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-13 15:46:48 +0100

    cambiar de hidden a none para ver

--- ejercicios/EjercicioJS/ejercio17arkaitz.js
+++ ejercicios/EjercicioJS/ejercio17arkaitz.js
@@ -1,23 +1,56 @@
-document.getElementById("primero").addEventListener("click", function(borrar){
+document.getElementById("enlace1").addEventListener("click", function(borrar){
+    if (document.getElementById("parafo1").style.display === "none")
+        {
+        document.getElementById("parafo1").style.display = "block";
+        document.getElementById("enlace1").textContent = "Ocultar contenidos";
+    } else {
+        document.getElementById("parafo1").style.display = "none";
+        document.getElementById("enlace1").textContent = "Show";
+    }
+}
+    );
+
+document.getElementById("enlace2").addEventListener("click", function(borrar){
+    if (document.getElementById("parafo2").style.display === "hidden")
+        {
+        document.getElementById("parafo2").style.display = "block";
+        document.getElementById("enlace2").textContent = "Ocultar contenidos";
+    } else {
+        document.getElementById("parafo2").style.display = "none";
+        document.getElementById("enlace2").textContent = "Show";
+    }
+}
+    );
+
+document.getElementById("enlace3").addEventListener("click", function(borrar){
+    if (document.getElementById("parafo3").style.display === "none")
+        {
+        document.getElementById("parafo3").style.display = "block";
+        document.getElementById("enlace3").textContent = "Ocultar contenidos";
+    } else {
+        document.getElementById("parafo3").style.display = "none";
+        document.getElementById("enlace3").textContent = "Show";
+    }
+}
+    );
+
+
+
+/*
+
+document.getElementById("enlace1").addEventListener("click", function(borrar){
+
     document.getElementById("parafo1").style.display = "none"
 });
 
-document.getElementById("segundo").addEventListener("click", function(borrar){
+
+document.getElementById("enlace2").addEventListener("click", function(borrar){
     document.getElementById("parafo2").style.display = "none"
 });
 
-document.getElementById("tercero").addEventListener("click", function(borrar){
+document.getElementById("enlace3").addEventListener("click", function(borrar){
     document.getElementById("parafo3").style.display = "none"
 });
 
-document.getElementById("primero").removeEventListener("click", function(borrar){
-    document.getElementById("parafo1").style.display = "none"
-});
 
-document.getElementById("segundo").removeEventListener("click", function(borrar){
-    document.getElementById("parafo2").style.display = "none"
-});
-
-document.getElementById("tercero").removeEventListener("click", function(borrar){
-    document.getElementById("parafo3").style.display = "none"
-});
+*/
