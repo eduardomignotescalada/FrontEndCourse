0e75abcf5ccab67726e2ea7289e9479b9310a92b
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-17 12:52:32 +0100

    correcion de los ejercicios

--- ejercicios/EjercicioJQuery/filtrosejercicios.js
+++ ejercicios/EjercicioJQuery/filtrosejercicios.js
@@ -9,11 +9,27 @@
 function init(){
     console.log("DOM Cargado Jquery");
     //modificando los atributos
+/*3. Haz que el tercer elemento del primer listado tenga el fondo amarillo. OK */
 $("ul:first li:nth-child(3)").css("background","yellow");
+
+/* 4. Haz que el cuarto elemento del primer listado tenga el fondo amarillo, pero seleccionando primero el tercer elemento. OK*/
 $("ul:first li:nth-child(3)").next().css("background","yellow");
-$("ul:eq(2)").prev().prev().attr("id", "iddelprimerul");;
+
+ /*5. Haz que el primer ul tenga un id pero entrando desde el tercer li del primer ul. */
+$("ul:first li:nth-child(3)").parent().attr("id", "iddelprimerul");
+/*5. Haz que el primer ul tenga un id pero entrando desde el tercer ul. */
+// $("ul:eq(2)").prev().prev().attr("id", "iddelprimerul");;
+
+/* 6. Haz que en el segundo listado todos los elementos tengan un fondo rojo pero entrando desde el primer elemento del segundo listado. OK*/
+
+/*  como lo hice yo
 $("ul:eq(1) li:first").css("background", "red");;
 $("ul:eq(1) li:first").nextAll().css("background", "red");;
+*/
+
+//como lo hace el profe cambiando por mi
+    $("ul:eq(1) li:first").parent().children().css("background-color","red");
+
 
 }
 
