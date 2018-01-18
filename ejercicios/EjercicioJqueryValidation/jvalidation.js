ecd225831c8d62a72935bf6fc8a5e72e36af9958
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-18 12:26:21 +0100

    validation del profe

--- ejercicios/EjercicioJqueryValidation/jvalidation.js
+++ ejercicios/EjercicioJqueryValidation/jvalidation.js
@@ -1,1 +1,27 @@
-test
+
+function init(){
+    $("#commentForm").validate({
+        rules: {
+            name: {
+                "required":true,
+                minlength:5
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required:"Por favor introduzca un nombre",
+                minlength: "Son necesarios al menos 5 caracteres!"
+            },
+            email: {
+                required: "Necesitamos su email para poder ponernos en contacto con usted",
+                email: "Su email debe tener el formato similar a usuario@dominio.com"
+            }
+        }
+    });
+    //Revisar la documentación del plugin para más datos https://jqueryvalidation.org/documentation/
+}
+$(document).ready(init);
