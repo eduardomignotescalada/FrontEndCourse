4f90122b8c2c0ff4186175dadc2363d5284365d3
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-29 17:58:48 +0100

    changed in exercises

--- ejercicios/EjercicioJS/ejercicioobjetos.js
+++ ejercicios/EjercicioJS/ejercicioobjetos.js
@@ -1,5 +1,4 @@
-/*
-OK 1.- Crea un html con un archivo externo JS asociado para resolver estos ejercicios
+/*OK 1.- Crea un html con un archivo externo JS asociado para resolver estos ejercicios
 2.- Crea un objeto Javascript que se llame cliente, dentro del objeto coloca las siguientes propiedades: nombre del cliente, direccion_fisica, email ,tlf y persona_contacto. Al crear el objeto colócale valores a cada propiedad.
 3.- Crea otro objeto con el nombre segundoCliente con las mismas propiedades pero con distintos valores.
 4.- Imprime por pantalla el nombre del primer objeto
@@ -26,21 +25,110 @@ OK 1.- Crea un html con un archivo externo JS asociado para resolver estos ejerc
 */
 
 
-function init() {
-    console.log("DOM Cargado Jquery");
-    var Cliente = function () {
-        this.nombre = "Juan";
-        this.direccion = "Calle Juan Bravo 49";
-        this.email = "juan.bravo@gmail.com";
-        this.tlf = "687 847 111";
-        this.contacto = "Pedro";
+/*function init() {
+    console.log("DOM Cargado Normal");*/
+
+
+// Primer intento con classe y constructor
+
+
+/* 2. Crea un objeto Javascript que se llame cliente, dentro del objeto coloca las siguientes propiedades: nombre del cliente, direccion_fisica, email ,tlf y persona_contacto. Al crear el objeto colócale valores a cada propiedad. */
+class Cliente {
+    constructor(nombre, direccion, email, tlf, contacto) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.email = email;
+        this.tlf = tlf;
+        this.contacto = contacto;
     }
-
-    var cliente1 = new Cliente();
-    var cliente2 = new Cliente("Pedro", "Calle Diego de Leon 49", "pedro.leon@gmail.com", "687 847 222");
-    console.log(cliente1.nombre);
-    console.log(cliente2.email);
-
 }
 
-$("document").ready(init);
+var clienteprimero = new Cliente('Juan', 'Calle Juan Bravo 49', 'juan.bravo@gmail.com', '687 847 111', 'Jaime');
+
+/* 3. Crea otro objeto con el nombre segundoCliente con las mismas propiedades pero con distintos valores. */
+var clientesegundo = new Cliente('Pedro', 'Calle Diego de Leon 49', 'pedro.leon@gmail.com', '687 847 222', 'Pablo');
+/* 4.- Imprime por pantalla el nombre del primer objeto */
+console.log(clienteprimero.nombre);
+/* 5.- Imprime por pantalla el email del segundo objeto*/
+console.log(clientesegundo.email);
+/* 6.  Cambia el nombre del primero objeto por el valor "Maderas Peralta" */
+clienteprimero.nombre = "Maderas Peralta";
+/* 7.- Imprime el valor del nombre del primer objeto */
+console.log(clienteprimero.nombre);
+/* 8.- Cambia el email del segundo objeto por el valor "info@micasuci.com" */
+clientesegundo.email = "info@micasuci.com";
+/* 9.- Imprime el email por pantalla del segundo objeto */
+console.log(clientesegundo.email);
+/* 10.- Añade al primer objeto un método (función) llamado toString, dicho método no recibe parámetros de entrada y devuelve una cadena de caracteres con los datos de las propiedades de ese objeto, indicando el nombre del objeto el nombre de cada propiedad y el valor asociado a cada propiedad */
+Cliente.prototype.toString = function () {
+    this.toString();
+}
+/* 11.- Imprime por pantalla el valor devuelto por el método toString del primer objeto */
+console.log("'El nombre del primer cliente es " + clienteprimero.nombre.toString() + ", el email del primer cliente es " + clienteprimero.email.toString());
+/* 12.- Añade un método toString similar en el segundo objeto */
+/*  13.- Imprime por pantalla el valor devuelvo por el método toString del segundo objeto */
+console.log("'El nombre del segundo cliente es " + clientesegundo.nombre.toString() + ", el email del segundo cliente es " + clientesegundo.email.toString());
+/* 14.- Añade un método setter y otro getter al primero objeto por cada propiedad definida en él, sin contar los métodos. */
+/* 15.- Utiliza todos los métodos definidos en el primer objeto dentro de tu código JS */
+/* // NO ME SALE
+ Cliente.prototype.apellido = apellido;
+
+clienteprimero.setnombre("JuanGet");
+clienteprimero.setemail("juanget@gmail.com");
+console.log(clienteprimer.nombre); */
+
+/* 16.- Introduce una nueva propiedad en el objeto cliente (1º) llamada facturacion, su valor inicial será un array de 2 posiciones con dos elementos de tipo numérico 4 y 25*/
+Cliente.prototype.facturacion = [0, 0];
+clienteprimero.facturacion = [4, 25];
+/* 17.- Imprime por pantalla el valor de la primera posición del array almacenado en la propiedad facturacion del primer objeto*/
+console.log("el primer numero de la factura del primer cliente es " + clienteprimero.facturacion[0]);
+/* 18.- Haz un método dentro del primer objeto llamado addFactura, al que se le pase como parámetro un número, que almacene ese valor como una nueva posición al final del array de facturacion del primer objeto*/
+clienteprimero.facturacion.push(2);
+/* 19.- Al imprimir por pantalla el número de elementos de array facturacion debería indicar que tiene 3 elementos*/
+console.log("El array tiene " + clienteprimero.facturacion.length + " elementos dentro del array")
+/* 20.- Crea un nuevo método en el primer objeto que sea capaz de devolver el total de facturación de ese objeto, total de los valores almacenados en el array facturacion del primero objeto.El método se llamara totalFacturacion*/
+var totalFactura1 = 0;
+for (var i = 0; i < 3; i++) {
+    totalFactura1 = totalFactura1 + clienteprimero.facturacion[i];
+}
+console.log("el total de factura es " + totalFactura1)
+/* 21.- Imprime por pantalla el valor devuelto por la llamada al método totalFactuacion del primer objeto*/
+console.log(Cliente.prototype.facturacion);
+/* 22.- Coloca en el primer objeto una nueva propiedad llamada direccionFiscal, el valor de esta propiedad será un objeto, las propiedades de este nuevo objeto serán, calle, ciudad, piso, puerta y CP.*/
+Cliente.prototype.direccionFiscal = function direccionFiscal(calle, ciudad, piso, puerta, CP) {
+    this.calle = calle;
+    this.ciudad = ciudad;
+    this.piso = piso;
+    this.puerta = puerta;
+    this.CP = CP;
+}
+
+var clienteprimero.direccionFiscal = ("calle diego leon", "madrid", "3", "4", "28028")
+/* 23.- Imprime por pantalla el valor de la ciudad de la dirección fiscal del primer objeto*/
+console.log(clienteprimero.direccionFiscal.ciudad)
+/* 24.- Cambia el CP de la dirección fiscal del primer objeto e imprime por pantalla dicho valor.*/
+clienteprimero.direccionFiscal.CP = 28020;
+
+
+// Segundo intento con una funcion
+
+function Clientebis(nombre, direccion, email, tlf, contacto) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.email = email;
+    this.tlf = tlf;
+    this.contacto = contacto;
+}
+
+var cliente3 = new Clientebis("Paul", "Calle Castllo 49", "paul.thames@gmail.com", "687 847 333", "Elena");
+var cliente4 = new Clientebis("Marc", "Calle Diego de Leon 49", "marc.james@gmail.com", "687 847 444", "Paloma");
+
+console.log(cliente3.nombre);
+console.log(cliente4.nombre);
+
+
+
+/*
+}
+
+document.addEventListener("DOMContentLoaded", init);*/
