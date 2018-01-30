f564bfdf19773f4ff000c05ca4674115bcb97fda
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-30 12:18:12 +0100

    test commit with visual studio

--- ejercicios/EjercicioJS/ejercicioclase.js
+++ ejercicios/EjercicioJS/ejercicioclase.js
@@ -37,17 +37,17 @@ class Moto {
         this.km = km || "0";
     }
 }
-/* 4. Crea un objeto de la clase Moto llamado miburra */
+/* 4. Crea un objeto de la clase Moto llamado miburra  > probando un cambio*/
 var miburra = new Moto();
 
 /* 5.- Cambia la propiedad matrícula de miburra por el valor "SA1234H" */
 miburra.matricula = "SA1234H"
-/* 6.- Imprime por pantalla el valor de la matrícula de miburra */
+    /* 6.- Imprime por pantalla el valor de la matrícula de miburra */
 console.log("La matricula de miburra es " + miburra.matricula)
-/* 9.- Demuéstralo creando otro objeto de la clase Moto e imprimiendo este valor por pantalla */
+    /* 9.- Demuéstralo creando otro objeto de la clase Moto e imprimiendo este valor por pantalla */
 var miferarri = new Moto();
 console.log("Los Km por defecto de de miferarri es " + miferarri.km)
-/* 10.- Crea los getter y setter de km y matricula para la clase Moto */
+    /* 10.- Crea los getter y setter de km y matricula para la clase Moto */
 
 //Setter
 /* setKm(km){
@@ -77,11 +77,11 @@ class Animal {
 }
 /* 15.- Crea un objeto de la clase Aminal, llamado animalico */
 var animalico = new Animal()
-/* 16.- Imprime por pantalla el objeto animalico y la propiedad edad del objeto */
+    /* 16.- Imprime por pantalla el objeto animalico y la propiedad edad del objeto */
 console.log("La edad por defecto de mi animal es " + animalico.edad)
-/* 17.- Cambia la edad de animalico a 3 */
+    /* 17.- Cambia la edad de animalico a 3 */
 animalico.edad = 3
-/* 18.- Imprime por pantalla la edad de animalico */
+    /* 18.- Imprime por pantalla la edad de animalico */
 console.log("La edad una vez cambiada de mi animal es " + animalico.edad)
 
 
