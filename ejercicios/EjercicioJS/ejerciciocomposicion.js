b71f1d4d3704d59f0eb99f47b2e849ac120e99e6
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-01-30 17:57:31 +0100

    ejercicio composicion

--- ejercicios/EjercicioJS/ejerciciocomposicion.js
+++ ejercicios/EjercicioJS/ejerciciocomposicion.js
@@ -21,7 +21,7 @@
 /* 1.- Crea un fichero HTML relacionado con un fichero JS > OK */
 /* 2.- Define una clase llamada Alumno que tenga las propiedades nombre, dni, tlf, email*/
 /* 5.- Haz que el constructor de la clase Alumno permita meter los 4 valores a las 4 propiedades del objeto, haz que estos parámetros sean opcionales */
-var Alumno = function (nombre="", dni="", tlf="", email="") {
+var Alumno = function (nombre = "", dni = "", tlf = "", email = "") {
     this.nombre = nombre;
     this.dni = dni;
     this.tlf = tlf;
@@ -61,12 +61,35 @@ var Juan = new Alumno();
 
 
 /* 6.- Crea otro objeto de la clase Alumno pero utilizando el nuevo constructor de la clase, pasando los 4 parámetros */
+var pedro = new Alumno("Pedro", "21888872B", "687 847 333", "pedro.github@gmail.com")
+console.log("El email de Pedro es "+pedro.email)
 /* 7.- Crea la clase Nota que tenga dos atributos: valor, asignatura */
+/* 10.- Modifica el constructor de la clase Nota para que admita 2 parámetros, uno para cada propiedad. Los parámetros deben poder ser opcionales.  */
+var Nota = function (valor="", asignatura="") {
+    this.valor = valor;
+    this.asignatura = asignatura;
+    /* 9.- Mete los getter y los setter de la clase Nota */
+    this.setValor = function (valor) {
+        this.valor = valor;
+    };
+    this.setAsignatura = function (asignatura) {
+        this.asignatura = asignatura;
+    };
+    this.getvalor = function () {
+        return this.valor;
+    };
+    this.getAsignatura = function () {
+        return this.asignatura;
+    };
+}
 /* 8.- Crea un objeto de la clase Nota */
-/* 9.- Mete los getter y los setter de la clase Nota */
-/* 10.- Modifica el constructor de la clase Nota para que admita 2 parámetros, uno para propiedad. Los parámetros deben poder ser opcionales.  */
+var frances1 = new Nota();
 /* 11.- Crea un objeto de la clase Nota pero pasando los dos parámetro sen su creación */
+var mates1 = new Nota("10", "mates");
+console.log("La nota de la asignatura "+mates1.asignatura+"es "+mates1.valor);
 /* 12.- Mete un nuevo atributo en la clase alumno llamado notas. Este atributo tiene que ser del tipo Array. En el constructor inicialízalo por defecto a [] */
+Alumno.prototype.notas=[];
+console.log(Alumno);
 /* 13.- en el constructor añade un parámetro más que será el array de notas */
 /* 14.- Crea un getter y un setter para notas */
 /* 15.- Crea un método en Alumno que permita introducir una nueva nota (al final del array de notas) */
