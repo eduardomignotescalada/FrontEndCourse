0c73c8c5884db341db84e1bd70b2f35dc60bf148
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-06 15:15:03 +0100

    ejemplo indexed-db

--- Examen/examen.js
+++ Examen/examen.js
@@ -1,31 +1,4 @@
-
-/* Time set sin delay
-
-$("#busqueda").on("keyup change", function () {
-    var value = this.value;
-    $("#dom_element").text(value);
-    console.log("esta buscando")
-});
-*/
-
-/* Time set with delay */
-
-$("#busqueda").delay(800).on("keyup change", function () {
-    var value = this.value;
-    $("#dom_element").text(value);
-    console.log("esta buscando")
-});
-
-/* Time set with setTimeOut */
-
-
-
-$("#busqueda").delay(800).on("keyup change", function () {
-    var value = this.value;
-    $("#dom_element").text(value);
-});
-
-/* Time set with setTimeOut */
+/* Time set with setTimeOut
 
 function myFunction() {
     // Declare variables
@@ -45,3 +18,49 @@ function myFunction() {
         }
     }
 }
+*/
+
+/* Time set sin delay*/
+
+
+function init() {
+    console.log("DOM Cargado Jquery");
+    //filtrando por selector
+    $("#busqueda").on("keyup change", function () {
+        var value = this.value;
+        $("#dom_element").text(value);
+        console.log("esta buscando")
+    });
+
+}
+
+$("document").ready(init);
+
+var lista_articulos = $.get('http://www.mocky.io/v2/5a54ae822d00005f235b1cd2', function (data) {
+    $(".result").html(data);
+    alert("Load was performed.");
+});
+
+
+/* Time set with delay
+
+$("#busqueda").delay(8).on("keyup change", function () {
+    var value = this.value;
+    $("#dom_element").text(value);
+    console.log("esta buscando")
+});
+
+
+*/
+
+
+/* Time set with setTimeOut
+
+
+
+$("#busqueda").delay(800).on("keyup change", function () {
+    var value = this.value;
+    $("#dom_element").text(value);
+});
+
+*/
