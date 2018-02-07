55c5bfe8064e9f173f28ddb4bf296b7bb46b5197
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-07 18:02:23 +0100

    ejercicio drag 2

--- ejercicios/Ejercicio-html5/ejercicio-drag2.js
+++ ejercicios/Ejercicio-html5/ejercicio-drag2.js
@@ -1,45 +1,39 @@
-<script>
-        //functions defined in init
-        function comenzar(event) {
-            console.log("Comenzando");
-            //transfiriendo datos
-            event.dataTransfer.setData("text", event.target.id);
-        }
+//functions defined in init
+function comenzar(event) {
+    console.log("empezando");
+    //transfiriendo datos
+    event.dataTransfer.setData("iden", event.target.id);
+}
 
-        function sobrevolar(event) {
-            console.log("Sobrevolando");
-            //evitando problemas al soltar
-            event.preventDefault()
-        }
-
-
-        function soltar(event) {
-            console.log("soltando");
-            //evitando problemas al soltar
-            event.preventDefault()
-            //cogiendo datos
-            var data = event.dataTransfer.getData("text");
-            var midiv = document.getElementById(event.target.id);
-            console.log(midiv);
-        }
+function sobrevolar(event) {
+    console.log("Sobrevolando");
+    //evitando problemas al soltar
+    event.preventDefault()
+}
 
 
+function soltar(event) {
+    console.log("soltando");
+    //evitando problemas al soltar
+    event.preventDefault()
+    //cogiendo datos
+    var data = event.dataTransfer.getData("iden");
+    event.target.appendChild(document.getElementById(data))
+}
 
-        //function init
-        function init() {
-            console.log("Dom cargado");
-            var o1 = document.getElementById("item-origen-1");
-            var o2 = document.getElementById("item-origen-2");
-            var o3 = document.getElementById("item-origen-3");
-            var d1 = document.getElementById("item-destino-1");
-            var d2 = document.getElementById("item-destino-2");
-            var d3 = document.getElementById("item-destino-3");
-            o1.addEventListener("dragstart", comenzar);
-            o1.addEventListener("dragover", sobrevolar);
-            o1.addEventListener("drop", soltar);
-        }
-        document.addEventListener("DOMContentLoaded", init);
-    </script>
-</body>
 
-</html>
+//function init
+function init() {
+    console.log("Dom cargado");
+    var o1 = document.getElementById("item-origen-1");
+    var o2 = document.getElementById("item-origen-2");
+    var o3 = document.getElementById("item-origen-3");
+    var d1 = document.getElementById("destino");
+    o1.addEventListener("dragstart", comenzar);
+    o2.addEventListener("dragstart", comenzar);
+    o3.addEventListener("dragstart", comenzar);
+    d1.addEventListener("dragover", sobrevolar);
+    d1.addEventListener("drop", soltar);
+
+}
+document.addEventListener("DOMContentLoaded", init);
