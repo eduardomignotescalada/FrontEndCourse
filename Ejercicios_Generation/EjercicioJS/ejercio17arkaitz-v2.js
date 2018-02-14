b3589d1428a6f1239183d32810dcba8bf8627b9f
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-14 15:18:22 +0100

    ejercicio listado

--- Ejercicios_Generation/EjercicioJS/ejercio17arkaitz-v2.js
+++ Ejercicios_Generation/EjercicioJS/ejercio17arkaitz-v2.js
@@ -2,8 +2,10 @@ function init() {
     console.log("DOM Cargado Jquery");
     $(".enlaces").click(function () {
         $(this).prev().toggleClass("hide");
+        //prev es el anterior hermano pero si pones prev("p") para especificar el antiguo hermano
         if($(this).prev().hasClass("hide")) {
             this.innerHTML="show";
+            // en vez de innerHTML puedes utilizar HTML en Jquery
         }
         else {
             this.innerHTML="hide";
@@ -11,3 +13,5 @@ function init() {
     });
 }
 $("document").ready(init);
+
+
