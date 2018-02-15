97b92531cdebf8ca8f07dfb8af2279b0e6482bdb
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-15 12:37:13 +0100

    ejercio 2 listado con search

--- Ejercicios_Generation/Ejercicios_JQuery/listado-v2.js
+++ Ejercicios_Generation/Ejercicios_JQuery/listado-v2.js
@@ -1,13 +1,30 @@
-
 function gettingData() {
-     $.getJSON('http://www.mocky.io/v2/5a54ae822d00005f235b1cd2', function (ev) {
-            var resultado = ev;
-            //pintar por pantalla listado
-            for (var i in resultado) {
-                $('#listado').append(`<li>El título del libro es: ${resultado[i].titulo}|| El autor es: ${resultado[i].autor}`)
-            }
-        });
+    $.getJSON('http://www.mocky.io/v2/5a54ae822d00005f235b1cd2', function (ev) {
+        var resultado = ev;
+        //pintar por pantalla listado
+        for (var i in resultado) {
+            $('#listado').append(`<li>El título del libro es: ${resultado[i].titulo} y el autor es: ${resultado[i].autor}`)
+        }
+    });
     console.log("function gettingData used");
+}
+
+function filter() {
+    criterio = $("#myInputTitle").val();
+    console.log(criterio);
+    $("#resultados-filtrados").html("");
+    console.log("borrar");
+    $.getJSON("http://data.colorado.gov/resource/4ykn-tg5h.json?entitystatus=Good%20Standing&principalzipcode=80001",
+        function (resultados) {
+            var tiendas = resultados;
+            $.each(resultados, function (i, results) {
+                if (results.agentfirstname != undefined && results.agentfirstname.includes(criterio)) {
+                    $("#resultados-filtrados").append("<li>" + results.agentfirstname + "</li>");
+                }
+            });
+            console.log("presentar los resultados en HTML")
+
+        })
 }
 
 
@@ -24,7 +41,5 @@ $(document).ready(app.init);
 1.- Crear un código html con un JS asociado
 2.- Descargar los datos mediante uan petición AJAX desde una URL "http://www.mocky.io/v2/5a54ae822d00005f235b1cd2" JSON y colocarlos en una variable llamada datos
 3.- Presentar los datos en un formato listado (ul+li)
-4.- Presentar los datos en un formato de tabla (tr+td)
-5.- Presentar los datos en un formato grid (bootstrap+col-6)
-6.- Guardar los datos en indexeddb para luego consultarlos antes de hacer la petición ajax
+4.- Colocar una caja de búsqueda que sirva de filtro para los listados
 */
