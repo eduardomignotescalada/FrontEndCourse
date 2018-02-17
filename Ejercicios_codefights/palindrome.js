da784ece984754e7adb16576e05fc766125ab77f
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-17 11:29:53 +0100

    change if function  -palindrome

--- Ejercicios_codefights/palindrome.js
+++ Ejercicios_codefights/palindrome.js
@@ -2,5 +2,10 @@ function checkPalindrome(inputString) {
     var reversedinputString = inputString.split("").reverse().join("");
     console.log(inputString);
     console.log(reversedinputString);
-    Boolean(inputString == reversedinputString);
+    if (inputString == reversedinputString) {
+        return (true);
+        else {
+            return (false);
+        }
+    }
 }
