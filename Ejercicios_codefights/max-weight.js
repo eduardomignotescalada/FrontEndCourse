9c43faa3e1774efb8ef53d6c96934512f96d5e1c
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-21 11:18:12 +0100

    change in ejercicio

--- Ejercicios_codefights/max-weight.js
+++ Ejercicios_codefights/max-weight.js
@@ -5,15 +5,18 @@ function knapsackLight(value1, weight1, value2, weight2, maxW) {
     } else if (maxW >= Math.max(weight1, weight2)) {
         console.log("puede llevar cualquiera -  el que tenga más valor")
         return Math.max(value1, value2)
-    } else if (maxW >= Math.min(weight1, weight2))
-        if ((maxW > weight1) || (value1 > value2)) {
+    } else if (maxW >= Math.min(weight1, weight2)) {
+        if ((maxW >= weight1) && (value1 > value2)) {
             console.log("caso especial 1")
             return value1
+        } else if ((maxW >= weight2) && (value2 > value1)) {
+            console.log("caso especial 2")
+            return value2
+        } else if (maxW >= Math.min(weight1, weight2)) {
+            console.log("caso especial 3")
+            return Math.min(value1,value2);
         }
-    else if ((maxW > weight2) || (value2 > value1)) {
-        console.log("caso especial 2")
-        return value2
-    } else {
+    } else if (maxW < Math.min(weight1, weight2)) {
         console.log("no puede llevar ninguno")
         return 0
     }
@@ -23,3 +26,5 @@ knapsackLight(1, 10, 2, 5, 6);
 knapsackLight(10, 5, 6, 4, 8);
 knapsackLight(10, 5, 6, 4, 9);
 knapsackLight(5, 3, 7, 4, 6)
+knapsackLight(10, 2, 11, 3, 1)
+knapsackLight(15, 2, 20, 3, 2)
