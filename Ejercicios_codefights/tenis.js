80e62ce8513881a1adf54c5a39a782bd2b3d2eb2
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-21 19:54:35 +0100

    ejercicios code fight

---
+++ Ejercicios_codefights/tenis.js
@@ -0,0 +1,19 @@
+    function tennisSet(score1, score2) {
+        if (score1 > 7 || score2 > 7) {
+            return false; }
+
+            else if((score1 >= 5 || score2 >=5 7)) {
+
+            }
+            if  {
+                return false
+            } else if() {
+                return true
+            }
+        }
+    }
+
+
+    function tennisSet(6, 5);
+    function tennisSet(7, 7);
+    function tennisSet(7, 2);
